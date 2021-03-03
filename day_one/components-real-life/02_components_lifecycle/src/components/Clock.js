091abd577becf320dc8037260c6b5f5405e5d910
Manuel Togni <togni@intre.it>
2021-03-03 12:46:29 +0100

    added lifecycle example

---
+++ day_one/components-real-life/02_components_lifecycle/src/components/Clock.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        const tick = this.tick;
+        this.timerID = setInterval(
+            () => tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>Ciao, mondo!</h1>
+          <h2>Sono le {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
+  
+  export default Clock
