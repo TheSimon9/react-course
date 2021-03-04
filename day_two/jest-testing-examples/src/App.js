86e8bc5bc95a172ede0d89f0c46a065118846373
Christian De Simone <christian.desimone@intre.it>
2021-03-04 18:11:00 +0100

    Test fake api call

--- day_two/jest-testing-examples/src/App.js
+++ day_two/jest-testing-examples/src/App.js
@@ -1,13 +1,36 @@
 import SoftwareDeveloper from "./components/SoftwareDeveloper";
-import {hireSoftwareEngineer} from "./utilities/utilities";
+import Weather from "./components/Weather";
+import {getWeather} from "./utilities/weather";
+import {useEffect, useState} from "react";
+
 
 function App() {
+
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    getWeather("Bergamo")
+      .then((data) => {
+        console.log(data);
+        setWeatherData(data);
+      });
+  }, [])
+
+
   return (
     <div className="App">
       <SoftwareDeveloper
         name={"Christian"}
         surname={"De Simone"}
         role={"Software Engineer"}/>
+
+      {weatherData &&
+      <Weather isSun={weatherData.current.condition.text === "Sunny"}
+               icon={weatherData.current.condition.icon}
+               city={weatherData.location.name}
+               country={weatherData.location.region}
+               temperature={weatherData.current.temp_c}/>
+      }
     </div>
   );
 }
