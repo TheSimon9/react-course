1e6936f2dd436747771741931328bb1ba5bfd5a8
Manuel Togni <togni@intre.it>
2021-03-12 13:13:15 +0100

    prettified

--- day_one/components-real-life/04_controlled_components/src/components/Item.js
+++ day_one/components-real-life/04_controlled_components/src/components/Item.js
@@ -1,23 +1,28 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default function Item(props) {
-    const [currentValue, setCurrentValue] = useState(props.value)
+  const [currentValue, setCurrentValue] = useState(props.value);
 
-    const editAction = () => props.setItemBeingEdited(props.index)
-    const onChange = e => setCurrentValue(e.target.value)
+  const editAction = () => props.setItemBeingEdited(props.index);
+  const onChange = (e) => setCurrentValue(e.target.value);
 
-    const keyPressed = (e) => {
-        if (e.code === "Enter") {
-            props.onChange(props.index, currentValue)
-        }
+  const keyPressed = (e) => {
+    if (e.code === "Enter") {
+      props.onChange(props.index, currentValue);
     }
+  };
 
-    if (props.inEdit) return <input value={currentValue} onChange={onChange} onKeyPress={keyPressed}/>
-
+  if (props.inEdit)
     return (
-        <div className={"item"}>
-            <div className={"item-value"}>{props.value}</div>
-            <div className={"edit-action"} onClick={editAction}>edit</div>
-        </div>
-    )
+      <input value={currentValue} onChange={onChange} onKeyPress={keyPressed} />
+    );
+
+  return (
+    <div className={"item"}>
+      <div className={"item-value"}>{props.value}</div>
+      <div className={"edit-action"} onClick={editAction}>
+        edit
+      </div>
+    </div>
+  );
 }
